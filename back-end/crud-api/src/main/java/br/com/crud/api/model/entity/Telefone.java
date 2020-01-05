95284e57a2d097410d3d8a5a5471dfe38af00178
Rafael Barros <rafaelbarros.df@gmail.com>
2020-01-05 20:58:56 -0300

    :construction: Criando entity Telefone

---
+++ back-end/crud-api/src/main/java/br/com/crud/api/model/entity/Telefone.java
@@ -0,0 +1,32 @@
+package br.com.crud.api.model.entity;
+
+
+import br.com.crud.api.model.enums.TipoTelefone;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O Campo tipo telefone é obrigatório")
+    private TipoTelefone tipoTelefone;
+
+    @NotNull(message = "O Campo número é obrigatório")
+    private String numero;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+}
