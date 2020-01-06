8d2b2f9b9b93c0e0a2d1405dc9a53896f70a6a50
Rafael Barros <rafaelbarros.df@gmail.com>
2020-01-05 21:43:20 -0300

    :fire: Removendo imports não utilizados

--- back-end/crud-api/src/main/java/br/com/crud/api/model/entity/Cliente.java
+++ back-end/crud-api/src/main/java/br/com/crud/api/model/entity/Cliente.java
@@ -1,13 +1,11 @@
 package br.com.crud.api.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
