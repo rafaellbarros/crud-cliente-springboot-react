498dd5c839f6a2e559a03e0683fa8f63f00f0dcd
Rafael Barros <rafaelbarros.df@gmail.com>
2020-01-06 08:03:03 -0300

    :construction: Adicionando Componentes

--- front-end/crud-reactjs-ui/src/App.js
+++ front-end/crud-reactjs-ui/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppRouter from "./component/RouterComponent";
+import NavBar from "./component/Navbar";
+import Container from '@material-ui/core/Container';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+            <Container>
+                <AppRouter/>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
